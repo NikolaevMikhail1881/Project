c6bcf781388acb8dc02a827024c183e5cf370d81
ShAmRoWw <110055739+ShAmRoWw@users.noreply.github.com>
2023-04-25 23:13:51 +0300

    Delete usercheck.h

--- usercheck.h
+++
@@ -1,9 +0,0 @@
-#ifndef USERCHECK_H
-#define USERCHECK_H
-
-#include <QString>
-
-bool registerUser(const QString& username, const QString& password);
-bool authorizeUser(const QString& username, const QString& password);
-
-#endif // USERCHECK_H
