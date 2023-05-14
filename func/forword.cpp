9c6360c2d9ad015e09252f3cd95a5abb0e8412a2
Артём Ванин <v.artyo@mail.ru>
2023-05-14 23:31:05 +0300

    func

---
+++ func/forword.cpp
@@ -0,0 +1,27 @@
+
+void table(int n){
+    int *noun {new int[n]};
+    int *verb {new int[n]};
+    int *adj {new int[n]};
+    int *pretext {new int[n]};
+    int *article {new int[n]};
+    int *period {new int[n]};
+    int *sign {new int[n]};
+    int *pronoun {new int[n]};
+    int *and_but {new int[n]};
+    int *els {new int[n]};
+
+    
+}
+
+int entering_arr(int arr[], int n){
+    for (int i = 0; i < n; i++){
+        std::cin >> arr[i];
+    }
+    return arr[];
+}
+
+int main(){
+    std::cout << table(10);
+
+}
